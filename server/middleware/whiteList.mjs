8bad26682e9898cab624b5a9cf458cb2e67b5b1b
timmy08808 <timmy08808@gmail.com>
2024-03-15 18:18:44 +0800

    referer

--- server/middleware/whiteList.mjs
+++ server/middleware/whiteList.mjs
@@ -3,10 +3,12 @@ import { whiteList } from '../config/index.mjs'
 export const whiteListMiddleware = () => {
     return async (ctx, next) => {
         let { referer } = ctx.header
-        referer = referer.slice(0, referer.length - 1)
-        if (!whiteList.includes(referer)) {
-            ctx.return({ code: 498, msg: '拒绝访问，请联系管理员' })
-            return
+        if (referer) {
+            referer = referer.slice(0, referer.length - 1)
+            if (!whiteList.includes(referer)) {
+                ctx.return({ code: 498, msg: '拒绝访问，请联系管理员' })
+                return
+            }
         }
         await next()
     }
