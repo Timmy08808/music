5da48731d52690bc4a1fc00cbe62c881a465e34c
timmy08808 <timmy08808@gmail.com>
2024-03-15 18:10:01 +0800

    tt

--- server/middleware/whiteList.mjs
+++ server/middleware/whiteList.mjs
@@ -5,7 +5,7 @@ export const whiteListMiddleware = () => {
         let { referer } = ctx.header
         referer = referer.slice(0, referer.length - 1)
         if (!whiteList.includes(referer)) {
-            ctx.return({ code: 498, msg: '域名不在白名单内，请联系管理员' })
+            ctx.return({ code: 498, msg: '拒绝访问，请联系管理员' })
             return
         }
         await next()
