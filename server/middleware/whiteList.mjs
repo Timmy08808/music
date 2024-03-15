3ba3cb42fee913337ee6c138c68a4506642f10a9
timmy08808 <timmy08808@gmail.com>
2024-03-15 18:00:56 +0800

    referer

--- server/middleware/whiteList.mjs
+++ server/middleware/whiteList.mjs
@@ -2,8 +2,9 @@ import { whiteList } from '../config/index.mjs'
 
 export const whiteListMiddleware = () => {
     return async (ctx, next) => {
-        const { origin } = ctx.header
-        if (!whiteList.includes(origin)) {
+        let { referer } = ctx.header
+        referer = referer.slice(0, referer.length - 1)
+        if (!whiteList.includes(referer)) {
             ctx.return({ code: 498, msg: '域名不在白名单内，请联系管理员' })
             return
         }
